e4c1b6ab347ea9d4bcef617d17995e77db991a04
Viktoras Navidauskas <vknavi@gmail.com>
2015-11-29 05:20:47 +0200

    update 1..01

--- GGDB/app/src/main/java/com/example/viktoras/ggdb/MainActivity.java
+++ GGDB/app/src/main/java/com/example/viktoras/ggdb/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.viktoras.ggdb;
 
 import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
